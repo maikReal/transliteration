6a74263d3610c9829a0a5f807756d6093460078d
maikReal <imaikal13998@gmail.com>
2019-02-27 17:59:18 +0300

    Updated transliteration rules

--- Transliteration/Transliteration/Program.cs
+++ Transliteration/Transliteration/Program.cs
@@ -8,7 +8,9 @@ namespace Transliteration
     {
         public static string transliteration(string word)
         {
-            Dictionary<string, string> rules = new Dictionary<string, string>
+            Dictionary<string, string> mainRule;
+            
+            Dictionary<string, string> rulesRUS_EN = new Dictionary<string, string>
             {
                 {"ее", "eye"},{"ое", "oye"},{"цы", "tsy"},{"сы", "sy"},{"ий", "y"},{"ые", "ye"},{"сыч", "sych"},
                 {"ё", "yo"},{"аев", "ayev"},{"ты", "ty"},{"лье", "lye"},{"ый", "y"},{"кс", "x"},{"ье", "e"},
@@ -18,8 +20,30 @@ namespace Transliteration
                 {"ш", "sh"},{"щ", "shch"},{"ъ", ""},{"ы", "yi"},{"ь", ""},{"э", "e"},{"ю", "yu"},{"я", "ya"}
             };
 
+            Dictionary<string, string> rulesEN_RUS = new Dictionary<string, string>
+            {
+                {"a","a"}, {"age","и"},{"ate","и"},{"cae","ка"},{"ae","и"},{"lay","лей"},{"ai","эй"},
+                {"ain","ен"},{"air","эр"},{"ayr","эр"},{"aire","эр"},{"ar","ар"},{"are","ар"},{"b","б"},
+                {"ca","ка"},{"ci","си"},{"cia","шия"},{"wich","вич"},{"chu","че"},{"chr","кр"},{"cha","ша"},
+                {"ck","л"},{"d","д"},{"set","сет"},{"en","ин"},{"ed","ид"},{"kea","ки"},{"rea","рей"},
+                {"ear","эр"},{"dear","дир"},{"ee","и"},{"eer","ир"},{"ere","ир"},{"peo","пи"},{"geon","жен"},
+                {"erd","эрд"},{"ew","юэ"},{"eu","ю"},{"ewe","ю"},{"ewer","юр"},{"key","ки"},{"hey","хей"},
+                {"f","ф"},{"ge","ж"},{"g","ж"},{"gg","гг"},{"h","х"},{"i","и"},{"ia","иа"},{"iar","айар"},
+                {"ie","айе"},{"ier","айер"},{"igh","ай"},{"ir","ир"},{"ire","айр"},{"k","к"},{"j","дж"},
+                {"ll","л"},{"m","м"},{"mpb","мпб"},{"n","н"},{"ng","нг"},{"oa","оу"},{"o","о"},{"oar","ор"},
+                {"oi","ой"},{"oy","ой"},{"oo","у"},{"oor","ор"},{"oore","ур"},{"or","ор"},{"ough","оу"},
+                {"our","аур"},{"ow","оу"},{"ower","оуэр"},{"ph","ф"},{"p","п"},{"qu","ку"},{"r","р"},{"s","с"},
+                {"sh","ш"},{"ss","сс"},{"t","т"},{"tch","тч"},{"ts","тс"},{"u","у"},{"ue","е"},{"v","в"},{"w","в"},
+                {"wor","уэр"},{"x","кс"},{"y","ай"},{"yr","эр"},{"z","з"}
+                
+            };
+
             word = word.ToLower();
-            foreach (var keyVal in rules)
+
+
+            mainRule = (int) word[0] < 127 ? rulesEN_RUS : rulesRUS_EN;
+
+            foreach (var keyVal in mainRule)
             {
                 word = word.Replace(keyVal.Key, keyVal.Value);
             }
@@ -30,7 +54,7 @@ namespace Transliteration
 
         public static void Main(string[] args)
         {
-            Console.WriteLine(transliteration("привет"));
+            Console.WriteLine(transliteration("hello"));
         }
     }
 }
